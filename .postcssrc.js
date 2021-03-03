07f6e9cae2b8a205b755c8243a9055b520251bc9
ranshuai <18210702905@163.com>
2021-03-03 11:35:32 +0800

    home

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,13 +6,13 @@ module.exports = {
         "postcss-url": {},
         // to edit target browsers: use "browserslist" field in package.json
         "autoprefixer": {
-            // overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+            overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
         },
         "postcss-pxtorem": {
-            // rootValue: 37.5,
-            // // Vant 官方根字体大小是 37.5
-            // propList: ['*'],
-            // selectorBlackList: ['.norem']
+            rootValue: 37.5,
+            // Vant 官方根字体大小是 37.5
+            propList: ['*'],
+            selectorBlackList: ['.norem']
             // 过滤掉.norem-开头的class，不进行rem转换
         }
     }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -42,8 +42,102 @@
             <div>热销商品</div>
             <div>更多商品</div>
         </section>
-        <section class="goods-card-small">
-            <div></div>
+        <section class="goods-card-small-wrapper">
+            <div class="goods-card-small">
+                <div></div>
+                <div>
+                    <div class="title">
+                        双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编
+                    </div>
+                    <div class="price">¥39.00</div>
+                </div>
+            </div>
+            <div class="goods-card-small">
+                <div></div>
+                <div>
+                    <div class="title">
+                        双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编
+                    </div>
+                    <div class="price">¥39.00</div>
+                </div>
+            </div>
+            <div class="goods-card-small">
+                <div></div>
+                <div>
+                    <div class="title">
+                        双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编双击编辑文本双击编
+                    </div>
+                    <div class="price">¥39.00</div>
+                </div>
+            </div>
+        </section>
+        <!-- title -->
+        <section class="my-title-wrapper">
+            <div>热门店铺</div>
+            <div>更多店铺</div>
+        </section>
+        <section class="shops-card-wrapper">
+            <div class="shops-card">
+                <div></div>
+                <div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>进入店铺</div>
+                </div>
+            </div>
+            <div class="shops-card">
+                <div></div>
+                <div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div>进入店铺</div>
+                </div>
+            </div>
+        </section>
+        <!-- title -->
+        <section class="my-title-wrapper">
+            <div>推荐商品</div>
+            <div>更多商品</div>
+        </section>
+        <section class="goods-card-big-wrapper">
+            <div class="goods-card-big">
+                <div></div>
+                <div>
+                    <div>双击编辑文本辑文本辑文本辑辑文本文本辑文本双击编</div>
+                    <div>起批量：100</div>
+                    <div>¥39.00</div>
+                </div>
+            </div>
+            <div class="goods-card-big">
+                <div></div>
+                <div>
+                    <div>双击编辑文本辑文本辑文本辑辑文本文本辑文本双击编</div>
+                    <div>起批量：100</div>
+                    <div>¥39.00</div>
+                </div>
+            </div>
+            <div class="goods-card-big">
+                <div></div>
+                <div>
+                    <div>双击编辑文本辑文本辑文本辑辑文本文本辑文本双击编</div>
+                    <div>起批量：100</div>
+                    <div>¥39.00</div>
+                </div>
+            </div>
+            <div class="goods-card-big">
+                <div></div>
+                <div>
+                    <div>双击编辑文本辑文本辑文本辑辑文本文本辑文本双击编</div>
+                    <div>起批量：100</div>
+                    <div>¥39.00</div>
+                </div>
+            </div>
         </section>
     </div>
 </template>
@@ -94,25 +188,108 @@ export default {
         @include flexbox();
         height: 56px;
         background: $white;
-        :first-child {
+        > :first-child {
             font-size: 18px;
             color: $titleFontColor;
         }
-        :last-child {
+        > :last-child {
             font-size: 12px;
             color: $fontTwo;
         }
     }
-    .goods-card-small {
-        :first-child {
-            width: 130px;
-            height: 130px;
-            background: #ccc;
+    .goods-card-small-wrapper {
+        @include marginBottom();
+        overflow: hidden;
+        @include flexbox();
+        .goods-card-small {
+            margin-left: 10px;
+            > :first-child {
+                width: 130px;
+                height: 130px;
+                background: #ccc;
+            }
+            > :last-child {
+                padding: 10px;
+                height: 70px;
+                width: 130px;
+                font-size: 12px;
+                background: $white;
+                @include flexbox(space-between, left, column);
+                .title {
+                    width: 100%;
+                    line-height: 14px;
+                    @include textoverflow(2);
+                }
+                .price {
+                    color: #ff0a0a;
+                }
+            }
         }
-        // :last-child {
-        //     height: 70px;
-        //     background: $white;
-        // }
+    }
+    .shops-card-wrapper {
+        overflow: hidden;
+        @include flexbox();
+        @include marginBottom();
+        .shops-card {
+            margin-left: 10px;
+            border-radius: 10px 10px 0 0;
+            > :first-child {
+                height: 120px;
+                background: $placeholder;
+            }
+            > :last-child {
+                padding: 20px 10px;
+                height: 150px;
+                background: $white;
+                > :first-child {
+                    @include flexbox();
+                    div {
+                        width: 64px;
+                        height: 64px;
+                        background: $placeholder;
+                    }
+                }
+                > :last-child {
+                    // padding: 10px 30px;
+                    border: 1px solid #ff0a0a;
+                    border-radius: 10px;
+                    color: #ff0a0a;
+                    font-size: 12px;
+                    width: 88px;
+                    height: 24px;
+                    text-align: center;
+                    line-height: 24px;
+                    margin: 0 auto;
+                    margin-top: 20px;
+                }
+            }
+        }
+    }
+    .goods-card-big-wrapper {
+        width: 100%;
+        @include flexbox(space-around, center, row, wrap);
+        .goods-card-big {
+            width: 175px;
+            > :first-child {
+                height: 130px;
+                background: $placeholder;
+            }
+            > :last-child {
+                font-size: 14px;
+                padding: 10px;
+                > :nth-child(1) {
+                    @include textoverflow(2);
+                    color: $fontOne;
+                }
+                > :nth-child(2) {
+                    color: $fontTwo;
+                    font-size: 12px;
+                }
+                > :nth-child(3) {
+                    color: $fontThree;
+                }
+            }
+        }
     }
 }
 </style>
